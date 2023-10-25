0dbd363c3592328b71217fc6e706143d3bde384b
Nathan Chung <nathanchung@live.com>
2023-10-24 23:25:18 -0400

    fixed last goal trajectory i think?

--- chung_navigate_to_goal/go_to_goal.py
+++ chung_navigate_to_goal/go_to_goal.py
@@ -3,9 +3,13 @@ from rclpy.node import Node
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Vector3, Twist, Point
 
+#rom rclpy.qos import QoSProfile, QoSDurabilityPolicy, QoSReliabilityPolicy, QoSHistoryPolicy
+
 import math
 import numpy as np
 import time
+
+# changed qos, odom callback. 
 
 class GoalController(Node):
     '''
@@ -28,7 +32,7 @@ class GoalController(Node):
         self.rotate_thr = 10 # rad
         self.linear_thr = 10 # rad
 
-        self.loop_rate = self.create_rate(0.1, self.get_clock())
+        #self.loop_rate = self.create_rate(0.1, self.get_clock())
 
         self.obstacle_subscriber = self.create_subscription(
             Vector3,
@@ -37,11 +41,17 @@ class GoalController(Node):
             5
         )
 
+        # #change qos below
+        # qos = QoSProfile(depth=5)
+        # qos.history = QoSHistoryPolicy.KEEP_LAST
+        # qos.durability = QoSDurabilityPolicy.VOLATILE 
+        # qos.reliability = QoSReliabilityPolicy.BEST_EFFORT 
+
         self.odom_subscriber = self.create_subscription(
             Odometry,
             '/odom',
             self.odom_callback,
-            1
+            1 #qos
         )
 
         self.move_publisher = self.create_publisher(Twist, '/cmd_vel', 5)
@@ -52,6 +62,11 @@ class GoalController(Node):
 
     def odom_callback(self, location):
         self.update_Odometry(location)
+        # delete below only for create 3 tests
+        # msg = Vector3()
+        # msg.x = -1.0
+        # state = self.get_state(msg)
+        # self.go_to_goal() # temporary
 
     def obstacle_callback(self, vector):
         state = self.get_state(vector)
@@ -72,7 +87,8 @@ class GoalController(Node):
         y_goal = abs(self.globalPos.y - self.goals[self.curr_index][1])
         if x_goal < 0.01 and y_goal < 0.01:
             self.curr_index += 1
-            self.loop_rate.sleep()
+            time.sleep(10)
+            #self.loop_rate.sleep()
         
         if vector.x == -1.0:
             return 0
@@ -80,16 +96,17 @@ class GoalController(Node):
             return 1
 
     def go_to_goal(self):
+        self.get_logger().info("-----------------------------------------------------------")
         target = self.goals[self.curr_index]
         my_location = self.globalPos
         my_orientation = self.globalAng
-        self.get_logger().info("target: " + str(target))
         dx = target[0] - my_location.x
         dy = target[1] - my_location.y
         target_distance = math.sqrt((dx ** 2) + (dy ** 2))
         #target_distance = math.sqrt(target[0] ** 2 + target[1] ** 2) - math.sqrt(my_location.x ** 2 + my_location.y ** 2)
         target_angle = math.atan2(dy,dx)
-
+        self.get_logger().info("target: " + str(target))
+        self.get_logger().info("target angle: " + str(target_angle))
         # try this if below doesnt work
         # margin = 0.04
         # if abs(self.globalAng) < math.pi + margin:
@@ -100,18 +117,23 @@ class GoalController(Node):
         #         if target_angle > 0:
         #             target_angle *= -1
 
-        if self.curr_index == 2:        
-            epos = target_angle - my_orientation
-            eneg = -target_angle + my_orientation #-target_angle - my_orientation
-            arr = np.array([epos, eneg])
-            self.get_logger().info("errors: " + str(arr) + " orientation: " + str(my_orientation))
-            e = arr[np.argmin(np.abs(arr))]
-            target_distance *= -1
-        else:
-            e = target_angle - my_orientation
+        # if self.curr_index == 2:        
+        #     epos = target_angle - my_orientation
+        #     eneg = -target_angle + my_orientation #-target_angle - my_orientation
+        #     arr = np.array([epos, eneg])
+        #     self.get_logger().info("error pos: %s, error neg: %s orientation: %s" %(epos * 180 / math.pi, eneg * 180 / math.pi, my_orientation* 180 / math.pi))
+        #     e = arr[np.argmin(np.abs(arr))]
+        #     target_distance *= -1
+        # else:
+        #     e = target_angle - my_orientation
 
-        #e = target_angle - my_orientation
-        self.get_logger().info(str(target_distance) + " " + str(e))
+        e = target_angle - my_orientation
+        if e > math.pi:
+            e -= 2*math.pi
+        elif e < -math.pi:
+            e += 2*math.pi
+        self.get_logger().info("target dist: " + str(target_distance) + " angle error: " + str(e*180/math.pi))
+        self.get_logger().info("error: %s, " %(str(e),))
         if abs(e) > 0.349: # roughly +/- 20 degrees
             kpa = 2
             ua = kpa * e
@@ -141,7 +163,7 @@ class GoalController(Node):
             ul = 0.0
             #ua = 0.0
         response = (ul, ua)
-        self.get_logger().info("sending: " + str(response))
+        #self.get_logger().info("sending: " + str(response))
         self.publish_message(response)
 
     def dodge_goal(self, vector): # wall running? idk i think it is lol. 
